0a4d327c122924142927ee473142b933c327aff7
SilJavJor <104878242+SilJavJor@users.noreply.github.com>
2023-04-28 18:02:02 -0300

    Actualizacion de Tarea 1 Ejercicio 1

---
+++ JavaScript/Tareas/Tarea1/Ejercicio1/ejercicio.js
@@ -0,0 +1,9 @@
+CalcularPiramide(43)
+
+function CalcularPiramide(numero) {
+    let listado = "";
+    for (let i = 1; i <= numero; i++) {
+      listado += i;
+      console.log(listado);
+    }
+}
